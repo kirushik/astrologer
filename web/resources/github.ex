3030ca89b37ef334e4b59c715c808fe6198d683a
Kirill Pimenov <kirill@pimenov.cc>
2016-01-02 23:53:14 +0100

    JSON parsing and URL pattern matching

--- web/resources/github.ex
+++ web/resources/github.ex
@@ -4,13 +4,18 @@ defmodule Astrologer.Github do
     @endpoint "https://api.github.com"
     @token Application.get_env(:astrologer, Astrologer.Endpoint)[:access_token]
 
-    defp process_url(url), do: @endpoint <> url
+    defp process_url(<<"http", _ :: binary>>=url), do: url
+    defp process_url(url), do: Path.join(@endpoint, url)
     defp process_request_headers(headers), do: headers ++ [{"Authorization", "token #{@token}"}]
+  end
+
+  defmodule StarredRepo do
+    defstruct [:full_name, :stargazers_count, :forks_count]
   end
 
   def starred do
     {:ok, %HTTPoison.Response{body: body}} = Client.get "/user/starred"
-    body
+    body |> Poison.decode!(as: [StarredRepo])
   end
 
 end
